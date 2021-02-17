2a76413a6d9bedb64e225c75d5fa814bb17abe83
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-17 15:33:37 -0500

    Código Fuente

--- src/main/java/edu/escuelaing/arep/app/Conexion.java
+++ src/main/java/edu/escuelaing/arep/app/Conexion.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class Conexion {
 	static String HTTPConnection(String lugar){
